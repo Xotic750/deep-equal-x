462453d6e04942561b7af973bc330e4b66702ad0
Graham Fairweather <xotic750@gmail.com>
2019-07-17 00:18:39 +0200

    Terraformed
    Auto build

--- __tests__/deep-equal-x.test.js
+++ __tests__/deep-equal-x.test.js
@@ -2,29 +2,6 @@
 
 let deepEqual;
 
-if (typeof module === 'object' && module.exports) {
-  require('es5-shim');
-  require('es5-shim/es5-sham');
-
-  if (typeof JSON === 'undefined') {
-    JSON = {};
-  }
-
-  require('json3').runInContext(null, JSON);
-  require('es6-shim');
-  const es7 = require('es7-shim');
-  Object.keys(es7).forEach(function(key) {
-    const obj = es7[key];
-
-    if (typeof obj.shim === 'function') {
-      obj.shim();
-    }
-  });
-  deepEqual = require('../../index.js');
-} else {
-  deepEqual = returnExports;
-}
-
 const hasBuffer = typeof Buffer === 'function';
 const ifBufferSupport = hasBuffer ? it : xit;
 let bufferFrom;
